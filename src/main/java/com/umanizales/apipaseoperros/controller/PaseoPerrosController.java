cdc3afc8698800b23491a86643f836c5418c5742
michael david herrada castañeda <mdherrada88032@umanizales.edu.co>
2021-05-01 15:03:47 -0500

    Initial commit primera parte funcional

---
+++ src/main/java/com/umanizales/apipaseoperros/controller/PaseoPerrosController.java
@@ -0,0 +1,33 @@
+package com.umanizales.apipaseoperros.controller;
+
+import org.springframework.validation.annotation.Validated;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/paseoperros")
+@Validated
+public class PaseoPerrosController {
+    @GetMapping
+    public String getMessage(){
+        return "hola maicol";
+    }
+    @GetMapping(path = "/message2")
+    public String getMessage2() {
+        return "hola maicol2";
+    }
+    @GetMapping(path = "/message/{saludo}")
+    public String getMessage3(@PathVariable("saludo") String saludo) {
+        return "hola maicol" + saludo;
+    }
+    @GetMapping(path = "/message/{saludo}/{saludo2}")
+    public String getMessage4(@PathVariable("saludo")String saludo,
+    @PathVariable("saludo2")String saludo2){
+        return saludo + saludo2;
+
+    }
+
+}
---
+++ src/main/java/com/umanizales/apipaseoperros/modelo/Empleado.java
@@ -0,0 +1,7 @@
+package com.umanizales.apipaseoperros.modelo;
+
+import java.io.Serializable;
+
+public class Empleado implements Serializable {
+
+}
