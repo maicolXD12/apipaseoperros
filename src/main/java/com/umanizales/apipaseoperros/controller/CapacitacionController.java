39d4b9db5d8313ed2f7ada424b46cd37e2237485
michael david herrada castañeda <mdherrada88032@umanizales.edu.co>
2021-05-01 16:37:31 -0500

    Empleados

--- src/main/java/com/umanizales/apipaseoperros/controller/CapacitacionController.java
+++ src/main/java/com/umanizales/apipaseoperros/controller/CapacitacionController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "/capacitacion")
 @Validated
@@ -20,5 +22,15 @@ public class CapacitacionController {
         return michael;
     }
 
-
+    @GetMapping("/empleados")
+    public Empleado[] getAllEmployees(){
+        Empleado[] empleados= new Empleado[3];
+        empleados [0]=new Empleado("Maicol herrada","10536563",
+                3000000);
+        empleados [1]=new Empleado("Eilyn","100343563",
+                4000000);
+        empleados [1]=new Empleado("pepito","100345463",
+                1000000);
+         return empleados;
+    }
 }
